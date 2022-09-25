a635ddae58b99f60d2dc0e3a5d86038933853a68
Ghita Aaddaj <aa.ghita@gmail.com>
2022-09-25 18:36:16 +0200

    random color for links:over

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,19 +1,8 @@
-// Visit The Stimulus Handbook for more details
-// https://stimulusjs.org/handbook/introduction
-//
-// This example controller works with specially annotated HTML like:
-//
-// <div data-controller="hello">
-//   <h1 data-target="hello.output"></h1>
-// </div>
-
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = []
 
   connect() {
-
     console.log('Hello from controller')
 
     function randomColor() {
@@ -24,9 +13,21 @@ export default class extends Controller {
       return 'rgb(' + color.join(', ') + ')';
     }
 
-    document.addEventListener("mouseover", function(){
-      document.a.style.backgroundColor = randomColor();
-    });
+    const links = document.querySelectorAll("#word");
+
+    console.log(links)
+
+    links.forEach(element => element.addEventListener("mouseover", (event) => {
+      console.log(element)
+      element.style.backgroundColor = randomColor();
+    })
+    )
+
+    links.forEach(element => element.addEventListener("mouseout", (event) => {
+      console.log(element)
+      element.style.backgroundColor = ''
+    })
+    )
   }
 
 }
