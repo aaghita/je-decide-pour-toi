aab5ba8d1e8ce6d170e8077c36c8c7509bc11c4a
Ghita Aaddaj <aa.ghita@gmail.com>
2022-10-12 00:13:06 +0200

    json extracted for mission

--- app/views/api/v1/humeurs/show.json.jbuilder
+++ app/views/api/v1/humeurs/show.json.jbuilder
@@ -1,3 +1,1 @@
-json.missions @mission do |mission|
-  json.extract! mission, :id, :humeur_id, :content, :address, :latitude, :longitude, :link, :video, :done, :image
-end
+json.extract! @mission, :id, :humeur_id, :content, :address, :latitude, :longitude, :link, :video, :done
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_10_103830) do
+ActiveRecord::Schema.define(version: 2022_10_11_213756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2022_10_10_103830) do
     t.string "link"
     t.string "video"
     t.boolean "done", default: false
+    t.string "title"
     t.index ["humeur_id"], name: "index_missions_on_humeur_id"
   end
 
